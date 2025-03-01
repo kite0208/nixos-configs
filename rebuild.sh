4212edd39fa41a84d6a4c83018f472bbc38ad895
kite0208 <kite0208@proton.me>
2025-03-01 14:14:42 +0100

    64 current  2025-03-01 14:11:05  25.05.20250216.d74a233     6.12.13                          *

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,7 @@ pushd ~/.dotfiles
 git add flake.nix
 git add system/
 git add user/
+git diff -U0
 echo "NixOS is rebuilding..."
 sudo nixos-rebuild switch --flake . && home-manager switch --flake . &>nixos-switch.log || (
   cat nixos-switch.log | grep --color error && false)
