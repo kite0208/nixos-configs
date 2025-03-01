8c46923e83f4713bdafd66d5218a2cc215ed27c8
kite0208 <kite0208@proton.me>
2025-03-01 14:22:28 +0100

    64 current  2025-03-01 14:11:05  25.05.20250216.d74a233     6.12.13                          *

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,10 @@
 set -e
 pushd ~/.dotfiles
-git diff -U0 flake.nix
-git diff -U0 system/
-git diff -U0 user/
+git diff -U0 flake.nix system user
 echo "NixOS is rebuilding..."
 sudo nixos-rebuild switch --flake . && home-manager switch --flake . &>nixos-switch.log || (
   cat nixos-switch.log | grep --color error && false)
 gen=$(nixos-rebuild list-generations | grep current)
-git commit -am "$gen"
+git commit -am "$gen" 
+git push origin main
 popd
