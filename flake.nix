f149d2c46836e499b0a3e3a12fc4e37ce267ae9e
kite0208 <kite0208@proton.me>
2025-03-01 13:45:18 +0100

    commit 1

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    
+    description = "flake";
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+	home-manager.url = "github:nix-community/home-manager/master";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	musnix.url = "github:musnix/musnix";
+	nvf.url = "github:notashelf/nvf";
+    };
+    outputs = { nixpkgs, home-manager, musnix, nvf, ... }@inputs: 
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in 
+	{
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit inputs;};
+                modules = [ 
+		        nvf.nixosModules.default
+			musnix.nixosModules.musnix
+			./system/configuration.nix
+	         ];
+            };
+	homeConfigurations = {
+		user = home-manager.lib.homeManagerConfiguration {
+		inherit pkgs;
+		modules = [ ./user/home.nix ];
+		};
+	};
+        };
+}
