2b750da52e1089228df74c7a473c70142f4bef8a
kite0208 <kite0208@proton.me>
2025-03-02 11:23:25 +0100

    65 current  2025-03-02 11:23:18  25.05.20250216.d74a233     6.12.13                          *

--- system/services.nix
+++ system/services.nix
@@ -11,6 +11,8 @@
     wantedBy = ["multi-user.target"];
     serviceConfig.Type = "simple";
   };
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
   services.fprintd.enable = true;
 
   # Cups
