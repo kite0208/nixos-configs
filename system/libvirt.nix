9571e81fd4cb74f32551c00a72672e527c582d9d
kite0208 <kite0208@proton.me>
2025-03-04 14:16:36 +0100

    70 current  2025-03-04 12:09:58  25.05.20250216.d74a233     6.12.13                          *

--- system/libvirt.nix
+++ system/libvirt.nix
@@ -3,24 +3,25 @@
   pkgs,
   ...
 }: {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          })
-          .fd
-        ];
-      };
-    };
-  };
+  virtualisation.xen.enable = true;
+  #  virtualisation.libvirtd = {
+  #    enable = true;
+  #    qemu = {
+  #      package = pkgs.qemu_kvm;
+  #      runAsRoot = true;
+  #      swtpm.enable = true;
+  #      ovmf = {
+  #        enable = true;
+  #        packages = [
+  #          (pkgs.OVMF.override {
+  #            secureBoot = true;
+  #            tpmSupport = true;
+  #          })
+  #          .fd
+  #        ];
+  #      };
+  #    };
+  #  };
   environment.systemPackages = [pkgs.virt-manager];
-  virtualisation.vmware.host.enable = true;
+  #  virtualisation.vmware.host.enable = true;
 }
